81485128ea0eb13079898affa58418626c0b6a5f
Real96 <chicco.gaeta@gmail.com>
2021-11-19 21:55:24 +0100

    Addded objects

--- BDSP_Yuzu_lua.lua
+++ BDSP_Yuzu_lua.lua
@@ -1,82 +1,99 @@
-local s0Addr = 0x258361810E0
-local s1Addr = 0x258361810E8
+local s0Addr = 0x20D001810E0
+local s1Addr = 0x20D001810E8
 
-local initS0 = readPointer(s0Addr)
-local initS1 = readPointer(s1Addr)
-local advances = 0
 
-print(string.format("Advances: %d", advances))
-print(string.format("S[0]: %016X  S[1]: %016X", initS0, initS1))
-print("")
 
-local aTimer = nil
-local timerInterval = 100
+XorShift = {}
+XorShift.__index = XorShift
 
-function next()
- local t = bAnd(initS0, 0xFFFFFFFF)
- local s = bShr(initS1, 32)
+function XorShift.new(s0, s1)
+ local o = setmetatable({}, XorShift)
+ o.s0 = s0
+ o.s1 = s1
+ o.advances = 0
+
+ return o
+end
+
+function XorShift:next()
+ local t = bAnd(self.s0, 0xFFFFFFFF)
+ local s = bShr(self.s1, 32)
 
  t = t ~ bAnd(bShl(t, 11), 0xFFFFFFFF)
  t = t ~ bShr(t, 8)
  t = t ~ (s ~ bShr(s, 19))
 
- initS0 = bOr(bShl(bAnd(initS1, 0xFFFFFFFF), 32), bShr(initS0, 32))
- initS1 = bOr(bShl(t, 32), bShr(initS1, 32))
+ self.s0 = bAnd(bOr(bShl(bAnd(self.s1, 0xFFFFFFFF), 32), bShr(self.s0, 32)), 0xFFFFFFFFFFFFFFFF)
+ self.s1 = bAnd(bOr(bShl(t, 32), bShr(self.s1, 32)), 0xFFFFFFFFFFFFFFFF)
+ self.advances = self.advances + 1
 
  return bAnd(((t % 0xFFFFFFFF) + 0x80000000), 0xFFFFFFFF)
 end
 
-function isShiny()
- local tempS0 = initS0
- local tempS1 = initS1
- local pid = next()
- local shinyRand = next()
+function XorShift:print()
+ print(string.format("Advances: %d", self.advances))
+ print(string.format("S[0]: %016X  S[1]: %016X", self.s0, self.s1))
+end
 
- initS0 = tempS0
- initS1 = tempS1
+local initRNG = XorShift.new(readPointer(s0Addr), readPointer(s1Addr))
+
+
+
+BDSPGenerator = {}
+BDSPGenerator.__index = BDSPGenerator
+
+function BDSPGenerator.new(s0, s1)
+ local o = setmetatable({}, BDSPGenerator)
+ o.currRNG = XorShift.new(s0, s1)
+
+ return o
+end
+
+function BDSPGenerator:isShiny()
+ local currRNG = XorShift.new(self.currRNG.s0, self.currRNG.s1)
+ local pid = currRNG:next()
+ local shinyRand = currRNG:next()
 
  return (bAnd(pid, 0xFFF0) ~ bShr(pid, 0x10) ~ bShr(shinyRand, 0x10) ~ bAnd(shinyRand, 0xFFF0)) < 0x10
 end
 
-function findShinyAdvances()
- local targetAdvances = 0
- local tempS0 = initS0
- local tempS1 = initS1
-
- while not isShiny() do
-  next()
-  targetAdvances = targetAdvances + 1
+function BDSPGenerator:printShinyAdvances()
+ while not self:isShiny() do
+  self.currRNG:next()
  end
 
- initS0 = tempS0
- initS1 = tempS1
-
- return targetAdvances
+ print(string.format("Next Shiny advances: %d", self.currRNG.advances))
+ print("")
 end
 
-local function showAdvances()
- local s0 = readPointer(s0Addr)
- local s1 = readPointer(s1Addr)
 
- while initS0 ~= s0 or initS1 ~= s1 do
-  next()
-  advances = advances + 1
 
-  if initS0 == s0 or initS1 == s1 then
-   print(string.format("Advances: %d", advances))
-   print(string.format("S[0]: %016X  S[1]: %016X", s0, s1))
-   print(string.format("Next Shiny advances: %d", findShinyAdvances()))
-   print("")
+local function printAdvances()
+ local currS0 = readPointer(s0Addr)
+ local currS1 = readPointer(s1Addr)
+
+ while currS0 ~= initRNG.s0 or currS1 ~= initRNG.s1 do
+  initRNG:next()
+
+  if currS0 == initRNG.s0 and currS1 == initRNG.s1 then
+   initRNG:print()
+   local generator = BDSPGenerator.new(currS0, currS1)
+   generator:printShinyAdvances()
   end
  end
 end
+
+
+
+local aTimer = nil
+local timerInterval = 100
 
 local function aTimerTick(timer)
  if isKeyPressed(VK_NUMPAD0) then
   timer.destroy()
  end
 
- showAdvances()
+ printAdvances()
 end
 
 aTimer = createTimer(getMainForm())
